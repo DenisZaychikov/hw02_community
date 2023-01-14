9b6a36eb3a07413f58e548f32d9b74385310837b
DenisZaychikov <denis.zaj4ikov@yandex.ru>
2023-01-14 12:24:06 +0300

    Create project

---
+++ yatube/posts/views.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from django.shortcuts import get_object_or_404, render
+
+from posts.models import Group, Post
+
+
+def index(request):
+    posts = Post.objects.all()[:settings.POSTS_AMOUNT]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.all()[:settings.POSTS_AMOUNT]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
